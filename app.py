8dea146526d9e488c0c35462293aea0595c1a21f
Jojo Java <jsjava.cvt@.eac.edu.ph>
2025-10-31 00:51:30 +0800

    some changes is added in app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def get_booked_dates_by_unit(room_id, start_date, end_date):
 
 def append_to_google_sheet(data):
     # Get JSON string from env var
-    creds_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+    creds_json = os.getenv("GOOGLE_CREDS_PATH")
     if not creds_json:
         print("⚠️ Google Sheets disabled: GOOGLE_SERVICE_ACCOUNT_JSON not set")
         return
